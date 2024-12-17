378404a7cd6f8edcacc96a488ac3013b0e292ca6
Kevin Brandt <keppe06@gmail.com>
2024-12-17 22:30:51 +0200

    Added route for index function

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,7 @@ Route::get('/', function () {
     return view('index'); 
 });
 
-Route::post('/booking/store', [BookingController::class, 'store'])->name('booking.store');
 
+
+Route::post('/booking/store', [BookingController::class, 'store'])->name('booking.store');
+Route::get('/booking/index', [BookingController::class, 'index'])->name('booking.index');
